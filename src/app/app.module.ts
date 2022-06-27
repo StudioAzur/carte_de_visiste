bb7ceea3cd6182500fca5e433fb200f55652db74
Wavelopper <alnetweb59@gmail.com>
2022-06-27 16:18:16 +0200

    [INITIALISATION] initialisation of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListeEmployesComponent } from './liste-employes/liste-employes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeEmployesComponent
   ],
   imports: [
     BrowserModule
